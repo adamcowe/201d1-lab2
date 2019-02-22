fb435374dac14ca21c41377037a0f78b979dbed9
ADAM S COWEN <adamcowe@amazon.com>
2019-02-22 13:18:07 -0800

    final version

--- app.js
+++ app.js
@@ -4,22 +4,22 @@
 
 //Questions//
 
-var pets = prompt('How many pets does Adam have?');
+var pets = prompt('Does Adam have pets?');
 console.log('Number of pets = ', pets);
 
-var stars = prompt('How many stars are in the sky?');
+var stars = prompt('Does Adam like staring at stars?');
 console.log('Number of the stars in the sky = ', stars);
 
-var salad = prompt('What is Adam\'s favorite Salad Dressing?');
+var salad = prompt('Does Adam like Blue Cheese Salad Dressing?');
 console.log('Adam\'s favorite Salad Dressing = ', salad);
 
-var cell= prompt('What sham cellphone does Adam have?');
+var cell= prompt('Does Adam hate his sham iPhone 6?');
 console.log('Adam\'s sham cellphone = ', cell);
 
-var talent = prompt('What is Adam\'s best talent?');
+var talent = prompt('Was Adam ever on a Talent Show?');
 console.log('Adam\'s best talent = ', talent);
 
-if (pets === '2' || stars.toLowerCase() === 'a lot' || salad === 'blue cheese' || cell === 'Apple 6s' || talent === '30 foot 3 pointers') {
+if (pets.toLowerCase() === 'y' || stars.toLowerCase() === 'y' || salad.toLowerCase() === 'y' || cell.toLowerCase() === 'n' || talent.toLowerCase() === 'n') {
   alert('You nailed it.');
 
   //If not all correct//
